a720c54369414775f1d8cb9cd8f40ea33b57faa7
wzj98 <67526681+wzj98@users.noreply.github.com>
2022-05-02 05:36:42 -0400

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -7,37 +7,35 @@ import random
 
 class ISBI_Loader(Dataset):
     def __init__(self, data_path):
-        # 初始化函数，读取所有data_path下的图片
+        
         self.data_path = data_path
         self.imgs_path = glob.glob(os.path.join(data_path, 'Training_Images/*.jpg'))
 
     def augment(self, image, flipCode):
-        # 使用cv2.flip进行数据增强，filpCode为1水平翻转，0垂直翻转，-1水平+垂直翻转
+        
         flip = cv2.flip(image, flipCode)
         return flip
         
     def __getitem__(self, index):
-        # 根据index读取图片
+        
         image_path = self.imgs_path[index]
-        # 根据image_path生成label_path
+        
         label_path = image_path.replace('Training_Images', 'Training_Labels')
-        label_path = label_path.replace('.jpg', '.png') # todo 更新标签文件的逻辑
+        label_path = label_path.replace('.jpg', '.png') 
 
-        # 读取训练图片和标签图片
-        # print(image_path)
-        # print(label_path)
+       
         image = cv2.imread(image_path)
         label = cv2.imread(label_path)
         image = cv2.resize(image, (512, 512))
         label = cv2.resize(label, (512, 512), interpolation=cv2.INTER_NEAREST)
-        # 将数据转为单通道的图片
+       
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         label = cv2.cvtColor(label, cv2.COLOR_BGR2GRAY)
 
-        # 处理标签，将像素值为255的改为1
+    
         if label.max() > 1:
             label = label / 255
-        # 随机进行数据增强，为2时不做处理
+      
         flipCode = random.choice([-1, 0, 1, 2])
         if flipCode != 2:
             image = self.augment(image, flipCode)
@@ -47,13 +45,13 @@ class ISBI_Loader(Dataset):
         return image, label
 
     def __len__(self):
-        # 返回训练集大小
+        
         return len(self.imgs_path)
 
     
 if __name__ == "__main__":
     isbi_dataset = ISBI_Loader("data/train/")
-    print("数据个数：", len(isbi_dataset))
+    
     train_loader = torch.utils.data.DataLoader(dataset=isbi_dataset,
                                                batch_size=2, 
                                                shuffle=True)
